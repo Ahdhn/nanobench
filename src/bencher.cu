5bdcb76b53473b32fcfcd179450b4530194ed4a1
ahdhn <eng.a.hassen@gmail.com>
2020-01-14 10:57:08 -0800

    Benchmarker for graph vs stream

--- src/bencher.cu
+++ src/bencher.cu
@@ -16,10 +16,10 @@ inline bool moveAndCompare(const int N, double* d_p, double* h_p_gold) {
     return compare_arr(h_p_gold, h_p_res.data(), N);
 }
 
-inline float DaxpyStream(const int N, double* d_r, double* d_p, const int num_iter,
-                         double* h_p_gold) {
+inline float DaxpyStream(const int N, double* d_r, double* d_p, const int num_ops,
+     double* h_p_gold) {
      //return the time in float 
-    double alpha = 1.0;    
+    double alpha = 1.0;        
     cudaStream_t cublas_stream;
     CUDA_ERROR(cudaStreamCreate(&cublas_stream));
     cublasHandle_t cublas_handle = 0;
@@ -32,7 +32,7 @@ inline float DaxpyStream(const int N, double* d_r, double* d_p, const int num_it
     CUDA_ERROR(cudaEventCreate(&start));
     CUDA_ERROR(cudaEventCreate(&stop));
     CUDA_ERROR(cudaEventRecord(start, cublas_stream));
-    for (int iter = 0; iter < num_iter; ++iter) {
+    for (int iter = 0; iter < num_ops; ++iter) {
         CUBLAS_ERROR(cublasDaxpy(cublas_handle, N, &alpha, d_r, 1, d_p, 1));    
         CUDA_ERROR(cudaStreamSynchronize(cublas_stream));
     }
@@ -55,34 +55,41 @@ inline float DaxpyStream(const int N, double* d_r, double* d_p, const int num_it
 
 
 
-    return time/ num_iter;
+    return time/ num_ops;
 
 }
-inline float DaxpyGraph(const int N, double*d_r, double *d_p, const int num_iter,
-                        double* h_p_gold) {
+
+inline float DaxpyGraph(const int N, double*d_r, double *d_p, const int num_ops,
+                        const int num_nodes, double* h_p_gold) {
     //return the time in float 
+    if(num_ops % num_nodes != 0){
+        fprintf(stderr, "DaxpyGraph():: num_ops shoudld be divisible by num_nodes");        
+        exit(EXIT_FAILURE);
+    }
+    
     double alpha = 1.0;
+    int num_runs = num_ops/num_nodes;
     cudaGraph_t cuda_graph;
     cudaStream_t capture_stream;
     cublasHandle_t cublas_handle = 0;
-
     CUBLAS_ERROR(cublasCreate(&cublas_handle));
     CUDA_ERROR(cudaStreamCreate(&capture_stream));
     CUDA_ERROR(cudaStreamBeginCapture(capture_stream, cudaStreamCaptureModeGlobal));
     CUBLAS_ERROR(cublasSetStream(cublas_handle, capture_stream));
     CUBLAS_ERROR(cublasSetPointerMode(cublas_handle, CUBLAS_POINTER_MODE_HOST));
-    CUBLAS_ERROR(cublasDaxpy(cublas_handle, N, &alpha, d_r, 1, d_p, 1));
+    for(int n=0;n<num_nodes;++n){
+        CUBLAS_ERROR(cublasDaxpy(cublas_handle, N, &alpha, d_r, 1, d_p, 1));
+    }
     CUDA_ERROR(cudaStreamEndCapture(capture_stream, &cuda_graph));
 
     cudaGraphNode_t* nodes = NULL;
-    size_t num_nodes = 0;
-    CUDA_ERROR(cudaGraphGetNodes(cuda_graph, nodes, &num_nodes));
-    if (num_nodes != 1) {
-        std::cout << " CUDA Graph has " << num_nodes << "!!" << std::endl;
+    size_t generated_num_nodes = 0;
+    CUDA_ERROR(cudaGraphGetNodes(cuda_graph, nodes, &generated_num_nodes));
+    if (generated_num_nodes != num_nodes) {
+        fprintf(stderr, "DaxpyGraph():: CUDA Graph has generated %d but the input is %d", 
+        static_cast<int>(generated_num_nodes), num_nodes);                
         exit(EXIT_FAILURE);
-    }
-    
-
+    }   
     cudaGraphExec_t cuda_graph_exec;
     CUDA_ERROR(cudaGraphInstantiate(&cuda_graph_exec, cuda_graph, NULL, NULL, 0));
 
@@ -90,7 +97,7 @@ inline float DaxpyGraph(const int N, double*d_r, double *d_p, const int num_iter
     CUDA_ERROR(cudaEventCreate(&start));
     CUDA_ERROR(cudaEventCreate(&stop));
     CUDA_ERROR(cudaEventRecord(start, capture_stream));
-    for (int iter = 0; iter < num_iter; ++iter) {
+    for (int iter = 0; iter < num_runs; ++iter) {
         CUDA_ERROR(cudaGraphLaunch(cuda_graph_exec, capture_stream));
         CUDA_ERROR(cudaStreamSynchronize(capture_stream));
     }
@@ -109,23 +116,24 @@ inline float DaxpyGraph(const int N, double*d_r, double *d_p, const int num_iter
     CUDA_ERROR(cudaEventElapsedTime(&time, start, stop));
 
     if (!moveAndCompare(N, d_p, h_p_gold)) {
-        std::cout << " DaxpyGraph() failed with N = " << N << std::endl;
+        fprintf(stderr, "DaxpyGraph():: failed with N= %d", N);                
         exit(EXIT_FAILURE);
     }
 
-    return time/ num_iter;
+    return time/ num_ops;
 }
 
-inline void benchDriver(const int num_iter = 100, const int start = 20,
-    const int end = 30) {
+inline void benchDriver(const int num_ops, const int num_nodes,
+                        const int start, const int end) {
 
-    std::cout << " ****** Bench Driver Started ******" << std::endl;   
+    std::cout << " ****** Bench Driver with "<<  num_nodes<< " nodes Started ******" << std::endl;   
     const char separator = ' ';
     const int numWidth = 15;
     std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << "Exp";
     std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << "Size";
     std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << "GraphTime";
-    std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << "StreamTime" << std::endl << std::endl;
+    std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << "StreamTime";
+    std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << "Speedup" << std::endl << std::endl;
     for (int exp = start; exp <= end; ++exp) {
         int N = 1 << exp;        
         double* d_r, * d_p;
@@ -139,7 +147,7 @@ inline void benchDriver(const int num_iter = 100, const int start = 20,
         std::generate(h_p.begin(), h_p.end(), []() {return double(rand()) / double(RAND_MAX); });
         
         std::vector<double> h_p_gold(h_p);
-        for (int iter = 0; iter < num_iter; ++iter) {
+        for (int iter = 0; iter < num_ops; ++iter) {
             for (int i = 0; i < N; ++i) {
                 h_p_gold[i] = h_r[i] + h_p_gold[i];
             }
@@ -147,21 +155,23 @@ inline void benchDriver(const int num_iter = 100, const int start = 20,
 
         CUDA_ERROR(cudaMemcpy(d_r, h_r.data(), N * sizeof(double), cudaMemcpyHostToDevice));
         CUDA_ERROR(cudaMemcpy(d_p, h_p.data(), N * sizeof(double), cudaMemcpyHostToDevice));
-        float graph_time = DaxpyGraph(N, d_r, d_p, num_iter, h_p_gold.data());
+        float graph_time = DaxpyGraph(N, d_r, d_p, num_ops, 10, h_p_gold.data());
 
         CUDA_ERROR(cudaMemcpy(d_r, h_r.data(), N * sizeof(double), cudaMemcpyHostToDevice));
         CUDA_ERROR(cudaMemcpy(d_p, h_p.data(), N * sizeof(double), cudaMemcpyHostToDevice));
-        float stream_time = DaxpyStream(N, d_r, d_p, num_iter, h_p_gold.data());
+        float stream_time = DaxpyStream(N, d_r, d_p, num_ops, h_p_gold.data());
 
         std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << exp;
         std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << N;
         std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << graph_time;
-        std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << stream_time << std::endl;        
+        std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << stream_time;
+        std::cout << std::left << std::setw(numWidth) << std::setfill(separator) << stream_time/graph_time << std::endl;        
+        
 
         CUDA_ERROR(cudaFree(d_r));
         CUDA_ERROR(cudaFree(d_p));
     }
-    std::cout << " ****** Bench Driver Finished ******" << std::endl;
+    std::cout << " ****** Bench Driver with "<<  num_nodes<< " nodes Started ******" << std::endl;   
 }
 
 
--- src/main.cu
+++ src/main.cu
@@ -5,12 +5,19 @@
 #include "cuda_query.h"
 
 int main(int argc, char** argv) {		
-	uint32_t device_id = 0;
+	uint32_t device_id = 3;
 	CUDA_ERROR(cudaSetDevice(device_id));
 	cuda_query(device_id);
 	
-	benchDriver(50, 1, 28);
+	benchDriver(200, 1, 1, 24);
+	benchDriver(200, 5, 1, 24);
+	benchDriver(200, 10, 1, 24);
+	benchDriver(200, 50, 1, 24);
+	benchDriver(200, 100, 1, 24);
+	benchDriver(200, 200, 1, 24);
 
-
+	/*for(int n = 1; n<50; ++n){
+		benchDriver(200, n, 1, 24);
+	}*/
 	return 0;
 }
