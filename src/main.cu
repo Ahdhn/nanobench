8dd73b4f9e9658f214d61ef0a09452d560d1dfde
ahdhn <eng.a.hassen@gmail.com>
2020-01-19 18:47:40 -0800

    increase size

--- src/main.cu
+++ src/main.cu
@@ -6,15 +6,15 @@
 #include "cuda_query.h"
 
 int main(int argc, char** argv) {		
-	uint32_t device_id = 0;
+	uint32_t device_id = 3;
 	double max_bandwidth = 0;	
 	CUDA_ERROR(cudaSetDevice(device_id));
 	cudaDeviceProp dev_props = cuda_query(device_id, max_bandwidth);
 	int sm_count = dev_props.multiProcessorCount;
 
-	axpyDriver(200, 1, 24, max_bandwidth, sm_count);
-	reduceDriver(200, 1, 24, max_bandwidth, ReduceOp::DOT, sm_count);
-	reduceDriver(200, 1, 24, max_bandwidth, ReduceOp::NORM2, sm_count);
+	//axpyDriver(200, 1, 28, max_bandwidth, sm_count);
+	reduceDriver(200, 1, 28, max_bandwidth, ReduceOp::DOT, sm_count);
+	reduceDriver(200, 1, 28, max_bandwidth, ReduceOp::NORM2, sm_count);
 
 
 	return 0;
