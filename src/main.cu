3928532f932b19c5ff19889de7d7a821f88d028d
Ahmed <eng.a.hassen@gmail.com>
2020-01-19 18:27:12 -0800

    missing file

--- src/main.cu
+++ src/main.cu
@@ -7,13 +7,14 @@
 
 int main(int argc, char** argv) {		
 	uint32_t device_id = 0;
-	double max_bandwidth = 0;
+	double max_bandwidth = 0;	
 	CUDA_ERROR(cudaSetDevice(device_id));
-	cuda_query(device_id, max_bandwidth);
-	
-	axpyDriver(200, 1, 30, max_bandwidth);
-	reduceDriver(200, 1, 30, max_bandwidth, ReduceOp::DOT);
-	reduceDriver(200, 1, 30, max_bandwidth, ReduceOp::NORM2);
+	cudaDeviceProp dev_props = cuda_query(device_id, max_bandwidth);
+	int sm_count = dev_props.multiProcessorCount;
+
+	axpyDriver(200, 1, 24, max_bandwidth, sm_count);
+	reduceDriver(200, 1, 24, max_bandwidth, ReduceOp::DOT, sm_count);
+	reduceDriver(200, 1, 24, max_bandwidth, ReduceOp::NORM2, sm_count);
 
 
 	return 0;
